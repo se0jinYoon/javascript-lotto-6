e1428a168ddeb64286b24e79dda0a9a2218c4ab5
se0jinYoon <tjwls1999@gmail.com>
2023-11-08 16:48:08 +0900

    feat : 구입 금액 입력 받기, 유효성 검증
    1. 유저로부터 구입 금액 입력 받는 메소드 구현
    2. 숫자인지, 1000단위인지 확인하는 메소드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { Console, Random } from '@woowacourse/mission-utils';
+import { check } from 'app';
+
 class App {
+  constructor() {
+    this.PURCHASE_AMOUNT = 0;
+  }
+
   async play() {}
+
+  // 구입 금액 입력 받기
+  async purchaseAmountInput() {
+    while (true) {
+      try {
+        const purchaseAmountInput = await Console.readLineAsync('구입 금액을 입력해 주세요.');
+        this.checkPurchaseAmount(purchaseAmountInput);
+        this.PURCHASE_AMOUNT = Number(purchaseAmountInput);
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  }
+
+  // 구입 금액 확인
+  checkPurchaseAmount(purchaseAmount) {
+    if (isNaN(this.PURCHASE_AMOUNT)) {
+      throw new Error('[ERROR] 숫자를 입력해주세요');
+    }
+    if (this.PURCHASE_AMOUNT % 1000) {
+      throw new Error('[ERROR]1000단위로 입력해주세요');
+    }
+  }
 }
 
 export default App;
